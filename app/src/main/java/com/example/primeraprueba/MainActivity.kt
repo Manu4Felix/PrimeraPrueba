297dd0c094d69af8a35055cc8387c52f87429637
Manu4Felix <manuel.felix.7e8@itb.cat>
2025-03-12 09:49:57 +0100

    Avance

--- app/src/main/java/com/example/primeraprueba/MainActivity.kt
+++ app/src/main/java/com/example/primeraprueba/MainActivity.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.collection.intIntMapOf
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -23,7 +30,10 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layout
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -36,43 +46,170 @@ class MainActivity : ComponentActivity() {
         setContent {
             PrimeraPruebaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Manuel",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+//                    Greeting(
+//                        name = "Manuel",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+                    Ejercicio1(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun ejercicio3(modifier: Modifier) {
+
+    Column(modifier.fillMaxSize()) {
+        Row(
+            modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Column(
+                modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color(0xFFEADDFF)),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    "Text composable",
+                    textAlign = TextAlign.Center,
+                    modifier = modifier.fillMaxWidth(),
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier.height(12.dp))
+                Text(
+                    "Displays text and follows the recommended Material Design guidelines",
+                    textAlign = TextAlign.Justify,
+                    modifier = modifier.padding(8.dp)
+                )
+
+            }
+            Column(
+                modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color(0xFFD0BCFF)),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    "Text composable",
+                    textAlign = TextAlign.Center,
+                    modifier = modifier.fillMaxWidth(),
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier.height(12.dp))
+                Text(
+                    "Displays text and follows the recommended Material Design guidelines",
+                    textAlign = TextAlign.Justify,
+                    modifier = modifier.padding(8.dp)
+                )
+
+            }
+
+        }
+        Row(
+            modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Column(
+                modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color(0xFFB69DF8)),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    "Text composable",
+                    textAlign = TextAlign.Center,
+                    modifier = modifier.fillMaxWidth(),
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier.height(12.dp))
+                Text(
+                    "Displays text and follows the recommended Material Design guidelines",
+                    textAlign = TextAlign.Justify,
+                    modifier = modifier.padding(8.dp)
+                )
+
+            }
+            Column(
+                modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color(0xFFF6EDFF)),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    "Text composable",
+                    textAlign = TextAlign.Center,
+                    modifier = modifier.fillMaxWidth(),
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier.height(12.dp))
+                Text(
+                    "Displays text and follows the recommended Material Design guidelines",
+                    textAlign = TextAlign.Justify,
+                    modifier = modifier.padding(8.dp)
+                )
+
+            }
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
 
     ConstraintLayout(modifier.fillMaxSize()) {
         val (boxRed, boxYellow, boxGreen, boxCyan, boxMagenta) = createRefs()
-        Box(modifier = Modifier.size(120.dp).background(Color.Red).constrainAs(boxRed){
-            top.linkTo(parent.top)
-            bottom.linkTo(parent.bottom)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-        })
-        Box(modifier = Modifier.size(120.dp).background(Color.Yellow).constrainAs(boxYellow){
-            start.linkTo(boxRed.start)
-            bottom.linkTo(boxRed.top)
-        })
-        Box(modifier = Modifier.size(120.dp).background(Color.Green).constrainAs(boxGreen){
-            start.linkTo(boxRed.start)
-            top.linkTo(boxRed.bottom)
-        })
-        Box(modifier = Modifier.size(120.dp).background(Color.Cyan).constrainAs(boxCyan){
-            end.linkTo(boxRed.start)
-            top.linkTo(boxRed.top)
-        })
-        Box(modifier = Modifier.size(120.dp).background(Color.Magenta).constrainAs(boxMagenta){
-            start.linkTo(boxRed.end)
-            top.linkTo(boxRed.top)
-        })
+        Box(
+            modifier = Modifier
+                .size(120.dp)
+                .background(Color.Red)
+                .constrainAs(boxRed) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                })
+        Box(
+            modifier = Modifier
+                .size(120.dp)
+                .background(Color.Yellow)
+                .constrainAs(boxYellow) {
+                    start.linkTo(boxRed.start)
+                    bottom.linkTo(boxRed.top)
+                })
+        Box(
+            modifier = Modifier
+                .size(120.dp)
+                .background(Color.Green)
+                .constrainAs(boxGreen) {
+                    start.linkTo(boxRed.start)
+                    top.linkTo(boxRed.bottom)
+                })
+        Box(
+            modifier = Modifier
+                .size(120.dp)
+                .background(Color.Cyan)
+                .constrainAs(boxCyan) {
+                    end.linkTo(boxRed.start)
+                    top.linkTo(boxRed.top)
+                })
+        Box(
+            modifier = Modifier
+                .size(120.dp)
+                .background(Color.Magenta)
+                .constrainAs(boxMagenta) {
+                    start.linkTo(boxRed.end)
+                    top.linkTo(boxRed.top)
+                })
     }
     Image(
         painter = painterResource(id = R.drawable.pepe),
@@ -83,11 +220,30 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 }
 
+@Composable
+fun Ejercicio1(modifier: Modifier = Modifier) {
+    Column(modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.background),
+            contentDescription = "Encabezado"
+        )
+    }
+    Column(modifier
+        .fillMaxSize()
+        .padding(horizontal = 12.dp)) {
+
+        Spacer(modifier.height(130.dp))
+        Text("Jetpack Compose tutorial")
+
+
+    }
+}
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun Pruebas() {
     PrimeraPruebaTheme {
-        Greeting("Manuel")
+        Ejercicio1(modifier = Modifier)
     }
 }
